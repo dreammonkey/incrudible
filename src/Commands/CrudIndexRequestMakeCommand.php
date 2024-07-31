4b0e3eec38466f9d29f1a4d167ff3b3cd4b129a9
Diederik van Remoortere <diederik.van.remoortere@gmail.com>
2024-07-31 22:15:24 +0200

    Add Spatie Laravel Permission package

--- src/Commands/CrudIndexRequestMakeCommand.php
+++ src/Commands/CrudIndexRequestMakeCommand.php
@@ -52,7 +52,7 @@ class CrudIndexRequestMakeCommand extends GeneratorCommand
     {
         return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
             ? $customPath
-            : __DIR__.'/../../resources'.$stub;
+            : __DIR__ . '/../../resources' . $stub;
     }
 
     /**
