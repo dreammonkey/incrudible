a127a2c3a04d4dda8355e066385381febdda5e75
Diederik van Remoortere <diederik.van.remoortere@gmail.com>
2024-08-14 18:15:44 +0200

    chore: Update AdminFactory to extend Factory<\App\Incrudible\Models\Admin>

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Incrudible\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Incrudible\Models\Admin>
  */
 class AdminFactory extends Factory
 {
