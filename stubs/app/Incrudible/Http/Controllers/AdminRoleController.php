51848a5d0a44aad08402326ffff40ba22989e129
dreammonkey <dreammonkey@users.noreply.github.com>
2024-08-14 22:27:57 +0000

    Fix styling

--- stubs/app/Incrudible/Http/Controllers/AdminRoleController.php
+++ stubs/app/Incrudible/Http/Controllers/AdminRoleController.php
@@ -2,8 +2,8 @@
 
 namespace App\Incrudible\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Incrudible\Models\Admin;
+use Illuminate\Http\Request;
 
 class AdminRoleController extends Controller
 {
